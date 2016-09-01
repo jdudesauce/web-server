f1cc178f855cbff554841e7e39cbb0d2bcf6868e
Brad Johnson <bjohnson@powerteq.com>
2016-09-01 14:34:04 -0600

    refactor middleware

--- server.js
+++ server.js
@@ -1,21 +1,10 @@
-var PORT = 3000;
 var express = require('express');
 var app = express();
+var PORT = 3000;
 
-var middleware = {
-	requireAuthentication: function (req, res, next) {
-		console.log('private route hit!');
-		next();
-	},
-	logger: function (req, res, next) {
-		console.log(req.method + ' ' + req.originalURL);
-		next();
-	}
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
-
-// app.use(middleware.requireAuthentication);
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
 	res.send('About Us~');
